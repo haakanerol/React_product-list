17aa2022fb51224ea3a3dd5e0e65c6184540f144
hakanerol <hakan-erol@outlook.com>
2024-07-25 23:57:24 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from "./components/header/Header";
 import ProductList from "./components/products/ProductList";
-import {products, categories} from "./helper/data";
-import "./App.scss"
+import { products, categories } from "./helper/data";
+import "./App.scss";
 function App() {
   // console.log(products,categories);
   return (
     <div className="App">
-      <Header categories={categories} text="Products List"/>
-      <ProductList products={products}/>
+      <Header categories={categories} text="Products List" />
+      <ProductList products={products} />
     </div>
   );
 }
