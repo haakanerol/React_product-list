3f8224b4c9fffda6dbd7576276bcf146dd7fbe82
hakanerol <hakan-erol@outlook.com>
2024-07-26 00:35:09 +0200

    up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,15 @@
+import HeaderStyles from "./Header.module.scss";
 
-import HeaderStyles from "./Header.module.scss"
-
-const Header=({categories,text})=>{
-   
-    
-    return(
-        <div className={HeaderStyles.header}>
-            <h1>{text}</h1>
-            <div className={HeaderStyles.btns}>
-            {
-                categories.map((item)=>(
-                    <button key={item}>{item}</button>
-                ))
-            }
-            </div>
-        </div>
-    )
-}
+const Header = ({ categories, text }) => {
+  return (
+    <div className={HeaderStyles.header}>
+      <h1>{text}</h1>
+      <div className={HeaderStyles.btns}>
+        {categories.map((item) => (
+          <button key={item}>{item}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Header;
